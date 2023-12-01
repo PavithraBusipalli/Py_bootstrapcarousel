e39f241300bf5482157bb8b4338ad6bb1d24742b
PavithraBusipalli <busipallipavitra@gmail.com>
2023-12-01 10:10:14 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def mdb_carousel(request):
+    return render(request,'mdb_carousel.htm')
+
+def carousel1(request):
+    return render(request,'carousel1.html')
